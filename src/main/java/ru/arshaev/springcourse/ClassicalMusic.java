24e06a36191c31597f1ec313233c8bba0f6b1d11
Bair Arshaev <ars-bair@yandex.ru>
2022-09-13 23:32:12 +0300

    Инверсия управления (IoC) на примере мини приложения Музыкальный плеер

---
+++ src/main/java/ru/arshaev/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.arshaev.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Бетховен";
+    }
+}
---
+++ src/main/java/ru/arshaev/springcourse/Music.java
@@ -0,0 +1,6 @@
+package ru.arshaev.springcourse;
+
+public interface Music {
+
+    String getSong();
+}
