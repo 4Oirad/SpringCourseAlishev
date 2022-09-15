ccca57c93532f813ce6c13c0a97ffa23d304bf9f
Bair Arshaev <ars-bair@yandex.ru>
2022-09-15 23:47:00 +0300

    DI with Setter and homework_add list

--- src/main/java/ru/arshaev/springcourse/MusicPlayer.java
+++ src/main/java/ru/arshaev/springcourse/MusicPlayer.java
@@ -1,23 +1,30 @@
 package ru.arshaev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+
+
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Играет: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Играет: " + music.getSong());
+        }
     }
 
     public String getName() {
---
+++ src/main/java/ru/arshaev/springcourse/PopMusic.java
@@ -0,0 +1,8 @@
+package ru.arshaev.springcourse;
+
+public class PopMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Руки Вверх - 18 мне уже!";
+    }
+}
