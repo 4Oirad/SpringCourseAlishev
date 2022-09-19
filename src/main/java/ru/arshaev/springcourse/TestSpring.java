5489a06a3baf871a774eb66462b24afba2084b9d
Bair Arshaev <ars-bair@yandex.ru>
2022-09-20 00:32:32 +0300

    Внедрение зависимостей с помошью аннотаций. Аннотация@Autowired

--- src/main/java/ru/arshaev/springcourse/TestSpring.java
+++ src/main/java/ru/arshaev/springcourse/TestSpring.java
@@ -9,15 +9,19 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//        Music music1 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+//
+//        musicPlayer.playMusic();
+//        musicPlayer1.playMusic();
 
-        Music music1 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
-
-        musicPlayer.playMusic();
-        musicPlayer1.playMusic();
-
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
